c33488cb89a9fe38ca2845808aa76872e84d8731
Dan Wojtowicz <itsdanwoj@gmail.com>
2022-05-04 13:34:37 -0500

    Add Pomofocus 12 hour format script

---
+++ scripts/pomofocus-12hr.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         Pomofocus 12-hour format
+// @namespace    http://danwoj.com/
+// @version      1.0
+// @description  Updates the Finish at time every 5 seconds to use a 12 hour format
+// @author       Dan Wojtowicz
+// @match        https://pomofocus.io/app
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    setInterval(function () {
+        let span = document.getElementsByClassName('sc-GMQeP kHCNYo')[2];
+        let militaryTime = span.innerText;
+        let hoursMinutes = militaryTime.split(':');
+        let hours = hoursMinutes[0];
+        let minutes = hoursMinutes[1]
+        let newHours = hours > 12 ? hours - 12 : (hours == 0 ? 12 : hours);
+        span.innerText = newHours + ":" + minutes;
+    }, 5000);
+})();
